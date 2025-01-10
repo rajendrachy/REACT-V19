2a4c53ff2a2b44e07309e66131ba2f66c0deb0a3
rajendrachy <chyrajendra32@gmail.com>
2025-01-10 23:27:12 +0530

    add lac 9

--- first-chy/src/App.jsx
+++ first-chy/src/App.jsx
@@ -11,36 +11,54 @@
 
 
 
-// Self close tag (/>)necessary for the not closing tags
+// -----------------Self close tag (/>)necessary for the not closing tags-------------------
 
+
+
+
+
+
+import React from "react";
+// or, Use -> import {Fragment} from "react";
+// and also change below ----> <React.Fragment>  to <Fragment> 
+// ----dont need to import and not used to write <React.Fragment> 
+
+// Only Use -> return (<> ....Function Components....</>)
+
+ 
 export const App = () => {
   return (
-  <div> 
-<Netflix></Netflix> 
-{/* components */}
-<Netflix></Netflix> 
-<Netflix></Netflix> 
-<Netflix></Netflix> 
-<Netflix></Netflix>  
-    </div>
+    // [<Netflix key="1"/><Netflix key="2"/>]
+    //<div>
+//---------------Using a react fragments to remove one extra div-----------------
+
+    <React.Fragment> 
+      <Netflix></Netflix>
+      <Netflix></Netflix>
+      <Netflix></Netflix>
+      <Netflix></Netflix>
+      <Netflix></Netflix>
+      {/* </div> */}
+    </React.Fragment>
   );
 }
 
 
 
-// Components -> Always Start with a Capital letter
+
+//------------------- Components -> Always Start with a Capital letter---------------------------------
 const Netflix = () => {
   return (
     <div>
 
-        <div>
-          <img src="images.jpeg" alt="images.jpeg"
-            width="100%" height="10%" />
-        </div>
-        <h2>Name: Queen of Tears</h2>
-        <h3>Rating: 8.2</h3>
-        <p>This is called </p>
+      <div>
+        <img src="images.jpeg" alt="images.jpeg"
+          width="100%" height="10%" />
       </div>
+      <h2>Name: Queen of Tears</h2>
+      <h3>Rating: 8.2</h3>
+      <p>This is called </p>
+    </div>
   );
 }
 
